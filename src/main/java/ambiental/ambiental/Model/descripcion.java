ab5248126352b4a0abb7821ba32c6f673b2a58d3
Alexis Rincón <marincon444@misena.edu.co>
2022-09-23 06:06:23 -0500

    Publicacion y descripcion

--- src/main/java/ambiental/ambiental/Model/descripcion.java
+++ src/main/java/ambiental/ambiental/Model/descripcion.java
@@ -1,12 +1,12 @@
 package ambiental.ambiental.Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name="descripcion")
